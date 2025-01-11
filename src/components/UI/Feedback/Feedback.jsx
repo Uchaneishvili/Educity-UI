f5c39742995a3d73123d69c9bffa58f1944f9477
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2025-01-11 16:27:42 +0400

    feedback colored stars are fixed

--- src/components/UI/Feedback/Feedback.jsx
+++ src/components/UI/Feedback/Feedback.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import styles from "./Feedback.module.css";
-import { FeedbackStar } from "../icons";
+import { ColoredStar, UncoloredStar } from "../icons";
 
 export function Feedback({ starsAmount }) {
   const stars = [];
   const emptyStars = 5 - starsAmount;
 
   for (let i = 0; i < starsAmount; i++) {
-    stars.push(<FeedbackStar key={i} />);
+    stars.push(<ColoredStar key={i} />);
   }
 
   for (let k = 0; k < emptyStars; k++) {
-    stars.push("empty");
+    stars.push(<UncoloredStar key={k} />);
   }
 
   return <div className={styles.feedbackStars}>{stars}</div>;

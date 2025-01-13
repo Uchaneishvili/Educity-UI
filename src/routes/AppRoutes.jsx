fd064058a2168262bb9ee10bd40b8e45b5c31f21
ItzVakko <itzvakkoofficial@gmail.com>
2025-01-13 17:43:08 +0400

    finished about us page for desktop

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
 import Contact from "../pages/Contact/Contact";
+import AboutUs from "../pages/AboutUs/AboutUs";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/contacts" element={<Contact />} />
+      <Route path="/aboutus" element={<AboutUs />} />
     </Routes>
   );
 };

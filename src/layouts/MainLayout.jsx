d17fa4146600e5330b224fef5f77b4609adcb9fe
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2025-01-18 11:49:24 +0400

    fix

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
-import { Header } from "../components/Header/Header";
-import { Footer } from "../components/Footer/Footer";
-import { Outlet } from "react-router-dom";
+import { Header } from '../components/Header/Header'
+import { Footer } from '../components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 export const MainLayout = () => {
   return (
@@ -10,6 +10,7 @@ export const MainLayout = () => {
         <Outlet />
       </main>
       <Footer />
+      {/*  */}
     </div>
-  );
-};
+  )
+}

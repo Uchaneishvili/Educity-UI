8207b7f57735a818baf18ef5fe81e2adbff1cb0e
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2025-03-16 21:46:37 +0400

    add messenger icon

--- src/components/Messenger/Messenger.jsx
+++ src/components/Messenger/Messenger.jsx
@@ -1,37 +1,22 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './Messenger.module.css';
 
 export function Messenger() {
-  useEffect(() => {
-    // Facebook SDK-ის ჩატვირთვა
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        appId: '1794676524647863', // თქვენი APP ID
-        xfbml: true,
-        version: 'v18.0',
-      });
-    };
-
-    // SDK-ის ასინქრონულად ჩატვირთვა
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = 'https://connect.facebook.net/ka_GE/sdk/xfbml.customerchat.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, 'script', 'facebook-jssdk');
-  }, []);
-
   return (
     <div className={styles.messengerContainer}>
-      <div id="fb-root"></div>
-      <div
-        className="fb-customerchat"
-        attribution="biz_inbox"
-        page_id="552099707988229" // თქვენი PAGE ID
-      ></div>
+      <a
+        href={`https://m.me/552099707988229`}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.messengerButton}
+        aria-label="Chat on Messenger"
+      >
+        <img
+          src="/assets/messenger-icon.png"
+          alt="Chat on Messenger"
+          className={styles.messengerIcon}
+        />
+      </a>
     </div>
   );
 }

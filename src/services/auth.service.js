346d0a746ebdb67a0be1b9320da6d65ea378afe4
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2025-03-27 00:05:57 +0400

    fix

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -224,13 +224,13 @@ class AuthService {
   }
 
   async facebookLogin() {
-    window.location.href = `${process.env.REACT_APP_API_URL}auth/facebook`;
-    // window.location.href = `https://api.educity.ge/api/v1/auth/facebook`;
+    // window.location.href = `${process.env.REACT_APP_API_URL}auth/facebook`;
+    window.location.href = `https://api.educity.ge/api/v1/auth/facebook`;
   }
 
   async googleLogin() {
-    // window.location.href = `https://api.educity.ge/api/v1/auth/google`;
-    window.location.href = `${process.env.REACT_APP_API_URL}auth/google`;
+    window.location.href = `https://api.educity.ge/api/v1/auth/google`;
+    // window.location.href = `${process.env.REACT_APP_API_URL}auth/google`;
   }
 }
 

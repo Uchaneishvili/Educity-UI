89f41a28489e47c9d5835e94bd47580485ce2746
ItzVakko <itzvakkoofficial@gmail.com>
2025-04-05 23:55:28 +0400

    finished course graduate swiper

--- src/components/CourseGraduateSwiper/CourseGraduateSwiper.jsx
+++ src/components/CourseGraduateSwiper/CourseGraduateSwiper.jsx
@@ -10,10 +10,10 @@ import 'swiper/css/navigation';
 const testimonials = [
   {
     id: 1,
-    image: '/assets/courseGraduates/courseGraduateGirlPhoto.png',
-    name: 'Gloria Rose',
-    text: "\"Thank you so much for your help. It's exactly what I've been looking for. You won't regret it. It really saves me time and effort. TOTC is exactly what our business has been lacking.\"",
-    rating: 3,
+    image: '/assets/courseGraduates/Ana-Kartozia.jpg',
+    name: 'ანა კარტოზია',
+    text: 'ვგრძნობ როგორ განვითარდი და უდიდეს მოტივაციას მაძლევს მენტორი, რაც ძალიან ბედნიერს მხდის!',
+    rating: 5,
   },
   {
     id: 2,
@@ -24,17 +24,31 @@ const testimonials = [
   },
   {
     id: 3,
-    image: '/assets/courseGraduates/courseGraduateGirlPhoto.png',
-    name: 'Gloria Rose',
-    text: "\"Thank you so much for your help. It's exactly what I've been looking for. You won't regret it. It really saves me time and effort. TOTC is exactly what our business has been lacking.\"",
-    rating: 3,
+    image: '/assets/courseGraduates/Lizi-Aminashvili.jpg',
+    name: 'ლიზი ამინაშვილი',
+    text: 'მენტორი არაჩვეულებრივია – მუდამ დადებითი, ენერგიული და მოტივაციის მომცემი. მიხარია რომ გავიცანი!',
+    rating: 5,
   },
   {
     id: 4,
-    image: '/assets/courseGraduates/courseGraduateGirlPhoto.png',
-    name: 'Gloria Rose',
-    text: "\"Thank you so much for your help. It's exactly what I've been looking for. You won't regret it. It really saves me time and effort. TOTC is exactly what our business has been lacking.\"",
-    rating: 3,
+    image: '/assets/courseGraduates/Medea-Tandashvili.jpg',
+    name: 'მედეა თანდაშვილი',
+    text: 'დიდი მადლობა ედუსითის გუნდს, იაკოსა და ლევანს სტუდენტებზე მუდმივი ზრუნვისა და მხარდაჭერისთვის!',
+    rating: 5,
+  },
+  {
+    id: 5,
+    image: '/assets/courseGraduates/Lana-Witelashvili.jpg',
+    name: 'ლანა წითელაშვილი',
+    text: 'მენტორი ქეთი პროფესიონალი და გულისხმიერია – დიდი მოტივაცია მაქვს მისი წყალობით და კურსმა მოლოდინებს გადააჭარბა!',
+    rating: 5,
+  },
+  {
+    id: 6,
+    image: '/assets/courseGraduates/Lika-Bedianidze.jpeg',
+    name: 'ლიკა ბედიანიძე',
+    text: 'მენტორი იაკო – სასწაული ლექტორია. ყოველთვის ყურადღებიანია, გულწრფელად ზრუნავს თითოეულ სტუდენტზე და ნებისმიერი სირთულის დროს გვერდში გვიდგას.',
+    rating: 5,
   },
 ];
 
